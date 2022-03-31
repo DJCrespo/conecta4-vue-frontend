02ee51048114d7b0c0cc8926e04d5d6b8b3617a6
Didier Crespo <dj.crespo.castilla@gmail.com>
2022-03-31 13:32:31 -0600

    add component "disc-item"

---
+++ components/discItem.vue
@@ -0,0 +1,21 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'disc-item',
+    props: {
+        'item-clicked': Boolean,
+        'click-function': Boolean
+    },
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
