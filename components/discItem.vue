5169ed6e9c6cf633185ace05616ef4704c1b66ed
Didier Crespo <dj.crespo.castilla@gmail.com>
2022-04-20 19:55:10 -0500

    board works

--- components/discItem.vue
+++ components/discItem.vue
@@ -1,16 +1,49 @@
 <template>
-    <div>
-        <h1>Hola</h1>
-    </div>
+  <div 
+    v-bind:class="[state == 0 || state == 1 ? 'tileclicked' : 'tile']"
+    v-bind:style="{
+      'background-color': state == 0 || state == 1 ? colors[state] : 'white',
+      'border': win ? 'solid 1.5px #e43437' : 'solid 1.5px lightgrey'
+    }"
+    v-on:click="handleClick"
+    >
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'disc-item',
-    data () {
-        return {
-
-        }
+  name: 'disc-item',
+  props: ['state', 'win', 'index'],
+  data () {
+    return {
+      colors: ['#febf33', '#344e5c']
     }
+  },
+  methods: {
+    handleClick() {
+      if (this.state == null) {
+        this.$emit('disc-clicked', this.index);
+      }
+    }
+  }
 }
 </script>
+
+<style>
+  .tile {
+    border-radius: 3.5vw;
+    height: 7vw;
+    width: 7vw;
+    max-height: 35px;
+    max-width: 35px;
+    cursor: pointer;
+  }
+  .tileclicked {
+    border-radius: 3.5vw;
+    height: 7vw;
+    width: 7vw;
+    max-height: 35px;
+    max-width: 35px;
+  }
+
+</style>
--- pages/board.vue
+++ pages/board.vue
@@ -1,13 +1,332 @@
 <template>
-    <disc-item></disc-item>
+  <div id="app">
+    <h1>connect 4</h1>
+
+    <div class="header">{{ playerNames[player] }}'s turn</div>
+
+    <!-- the bord -->
+    <table>
+      <tr v-for="(row, ridx) in boardConst">
+        <td v-for="(col, cidx) in row">
+          <disc-item
+            v-bind:state="col"
+            v-bind:win="winningDiscs[ridx][cidx]"
+            v-bind:index="{ ridx: ridx, cidx: cidx }"
+            v-on:disc-clicked="handleDisc"
+          >
+          </disc-item>
+        </td>
+      </tr>
+    </table>
+
+    <!-- a super simple modal -->
+    <div v-if="modalOpen">
+      <div class="backdrop"></div>
+      <div class="modal">
+        <div class="modalcontent">
+          {{ winner }} wins!
+          <button v-on:click="closeModal">ok</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
+
 export default {
-    
-}
+  name: "board",
+  props: {
+      players: Object
+  },
+  data() {
+    return {
+      dimension: { x: 7, y: 6 },
+      player: 0,
+      boardConst: null,
+      winningDiscs: null,
+      playerNames: ["orange", "blue"],
+      modalOpen: false,
+      winner: null,
+    };
+  },
+  mounted() {
+    this.initializeBoard();
+  },
+  methods: {
+    handleDisc(e) {
+      // reconoce la columna donde el jugador desea colocar la pieza, una vez colocada cambia de jugador
+      this.boardConst[e.ridx][e.cidx] = this.player;
+      console.log(this.boardConst);
+      const correctedArray = this.applyGravity(
+        this.boardConst,
+        this.dimension.x,
+        this.dimension.y
+      );
+      this.boardConst = correctedArray;
+
+      // switch player
+      this.player = (this.player + 1) % 2;
+      const result = this.checkWinCondition(
+        this.boardConst,
+        this.dimension.x,
+        this.dimension.y
+      );
+
+      // what to do when win happened
+      if (result.win) {
+        result.discs.forEach((d) => {
+          Vue.set(this.winningDiscs[d.row], d.col, true);
+        });
+        this.winner = this.playerNames[result.player];
+        this.modalOpen = true;
+        this.player = (result.player + 1) % 2;
+      }
+    },
+    closeModal() {
+      this.initializeBoard();
+      this.modalOpen = false;
+    },
+    initializeBoard() {
+      let res = [],
+        i,
+        j;
+      for (i = 0; i < this.dimension.y; i++) {
+        res.push([]);
+        for (j = 0; j < this.dimension.x; j++) {
+          res[i].push(null);
+        }
+      }
+      this.boardConst = res;
+      // initialize winning discs
+      res = [];
+      for (i = 0; i < this.dimension.y; i++) {
+        res.push([]);
+        for (j = 0; j < this.dimension.x; j++) {
+          res[i].push(false);
+        }
+      }
+      this.winningDiscs = res;
+    },
+    checkWinCondition(arr, dimx, dimy) {
+      let check1, check2, check3, i, j;
+
+      check1 = this.horizontalWin(arr, dimx, dimy);
+      if (check1.win) {
+        return check1;
+      } else {
+        check2 = this.verticalWin(arr, dimx, dimy);
+        if (check2.win) {
+          return check2;
+        } else {
+          for (i = 0; i < dimy; i++) {
+            for (j = 0; j < dimx; j++) {
+              check3 = this.diagonalWin(i, j, dimx, dimy, arr);
+              if (check3.win) {
+                return check3;
+              }
+            }
+          }
+        }
+      }
+
+      return { win: false };
+    },
+    horizontalWin(arr, dimx, dimy) {
+      let i, j, cnt, ply;
+      for (i = 0; i < dimy; i++) {
+        for (j = 0; j < dimx; j++) {
+          if (j === 0) {
+            ply = arr[i][j];
+            cnt = 1;
+          } else {
+            if (ply === arr[i][j] && ply != null) {
+              cnt = cnt + 1;
+              if (cnt === 4) {
+                return {
+                  win: true,
+                  player: ply,
+                  discs: [
+                    { row: i, col: j - 3 },
+                    { row: i, col: j - 2 },
+                    { row: i, col: j - 1 },
+                    { row: i, col: j },
+                  ],
+                };
+              }
+            } else {
+              ply = arr[i][j];
+              cnt = 1;
+            }
+          }
+        }
+      }
+      return { win: false };
+    },
+    verticalWin(arr, dimx, dimy) {
+      let i, j, cnt, ply;
+      for (i = 0; i < dimx; i++) {
+        for (j = 0; j < dimy; j++) {
+          if (j === 0) {
+            ply = arr[j][i];
+            cnt = 1;
+          } else {
+            if (ply === arr[j][i] && ply != null) {
+              cnt = cnt + 1;
+              if (cnt === 4) {
+                return {
+                  win: true,
+                  player: ply,
+                  discs: [
+                    { row: j - 3, col: i },
+                    { row: j - 2, col: i },
+                    { row: j - 1, col: i },
+                    { row: j, col: i },
+                  ],
+                };
+              }
+            } else {
+              ply = arr[j][i];
+              cnt = 1;
+            }
+          }
+        }
+      }
+      return { win: false };
+    },
+    diagonalWin(row, col, dimx, dimy, arr) {
+      let maxCol = dimx - 1;
+      let maxRow = dimy - 1;
+      let ply;
+      if (row + 3 <= maxRow && col + 3 <= maxCol) {
+        ply = arr[row][col];
+
+        if (
+          arr[row + 1][col + 1] === ply &&
+          arr[row + 2][col + 2] === ply &&
+          arr[row + 3][col + 3] === ply &&
+          ply !== null
+        ) {
+          return {
+            win: true,
+            player: ply,
+            discs: [
+              { row: row, col: col },
+              { row: row + 1, col: col + 1 },
+              { row: row + 2, col: col + 2 },
+              { row: row + 3, col: col + 3 },
+            ],
+          };
+        }
+      }
+
+      if (row + 3 <= maxRow && col - 3 >= 0) {
+        ply = arr[row][col];
+
+        if (
+          arr[row + 1][col - 1] === ply &&
+          arr[row + 2][col - 2] === ply &&
+          arr[row + 3][col - 3] === ply &&
+          ply !== null
+        ) {
+          return {
+            win: true,
+            player: ply,
+            discs: [
+              { row: row, col: col },
+              { row: row + 1, col: col - 1 },
+              { row: row + 2, col: col - 2 },
+              { row: row + 3, col: col - 3 },
+            ],
+          };
+        }
+      }
+      return false;
+    },
+    applyGravity(arr, dimx, dimy) {
+      var row, col;
+      var res = arr.slice();
+      for (col = 0; col < dimx; col++) {
+        for (row = 0; row < dimy; row++) {
+          if (row < dimy - 1) {
+            if (res[row][col] != null && res[row + 1][col] == null) {
+              res[row + 1][col] = res[row][col];
+              res[row][col] = null;
+            }
+          }
+        }
+      }
+      return res;
+    },
+  },
+};
 </script>
 
 <style>
+table,
+th,
+td {
+  border: 2px solid #b3b3b3;
+  border-collapse: collapse;
+  background-color: lightgrey;
+}
+table {
+  margin: auto;
+}
 
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: darkslategrey;
+  margin-top: 60px;
+}
+
+.header {
+  font-size: 4vw;
+  margin-bottom: 10px;
+}
+
+.backdrop {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  background-color: lightgrey;
+  opacity: 0.5;
+  top: 0;
+  left: 0;
+}
+
+.modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+
+.modalcontent {
+  margin-left: auto;
+  margin-right: auto;
+  /*margin-top: 40vh;*/
+  background-color: white;
+  width: 50vw;
+  opacity: 1;
+  height: 35px;
+  padding-top: 10px;
+  border-radius: 2px;
+  border: solid 1.5px grey;
+}
+
+button {
+  background-color: grey;
+  border: solid 1px;
+  border-radius: 2px;
+  outline: none;
+  color: white;
+  cursor: pointer;
+  margin-left: 10px;
+}
 </style>
